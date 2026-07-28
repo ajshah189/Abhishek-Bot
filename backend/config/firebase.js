e79d0b03cc764056e00354d8df28df646ead818f
Abhishek Shah <ajshah189@gmail.com>
2026-07-28 15:16:12 +0530

    Fix Firebase private key parsing

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,6 +1,3 @@
-import { config } from 'dotenv';
-config(); // Load dotenv first
-
 import admin from 'firebase-admin';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -11,13 +8,17 @@ const __dirname = dirname(__filename);
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   clientId: process.env.FIREBASE_CLIENT_ID,
   authUri: 'https://accounts.google.com/o/oauth2/auth',
   tokenUri: 'https://oauth2.googleapis.com/token',
   authProviderX509CertUrl: 'https://www.googleapis.com/oauth2/v1/certs'
 };
+
+if (!serviceAccount.projectId) {
+  throw new Error('FIREBASE_PROJECT_ID not set');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
